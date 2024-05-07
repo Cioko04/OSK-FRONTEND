b356333949c706833f35e96d3a95dfefdd97ce94
Patryk <brzepat@gmail.com>
2024-05-07 20:00:53 +0200

    Create menu item in ts file

--- src/app/home/user-dashboard/course-sign-up/course-sign-up-menu/course-sign-up-menu.component.ts
+++ src/app/home/user-dashboard/course-sign-up/course-sign-up-menu/course-sign-up-menu.component.ts
@@ -1,7 +1,12 @@
-import {
-  Component
-} from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
+export interface MenuItem {
+  imagePath: string;
+  type: string;
+  text: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-course-sign-up-menu',
@@ -9,8 +14,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./course-sign-up-menu.component.css'],
 })
 export class CourseSignUpMenuComponent {
+  menuItems: MenuItem[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    this.addMenuItems();
+  }
+
+  private addMenuItems() {
+    this.menuItems = [
+      {
+        imagePath: 'course-sing-in.png',
+        type: 'course',
+        text: 'Zapisz się na KURS',
+        route: 'choose-school',
+      },
+      {
+        imagePath: 'additional-lesson-sign-in.png',
+        type: 'lesson',
+        text: 'Zapisz się na DODATKOWE JAZDY',
+        route: 'payments',
+      },
+    ];
+  }
 
   navigate(route: string) {
     this.router.navigate([`/home/course-sign-up/${route}`]);
